49783d861d0de268292a94a0768a357eb5b465fd
acatthatprograms <acatthatprograms@gmail.com>
2020-05-27 01:06:19 -0400

    txt

--- src/util/narrative.js
+++ src/util/narrative.js
@@ -1,60 +1,56 @@
 let page0 =
-`Hello there! [2br] [w350] Have a seat, because have I got a story for you! [wi]
+`Hello there! [2br] [w350] Have a seat, I've got a story for you! [w350]
 [2br] Now, [w350] here’s a tale worthy enough for the bedtimes of all young software engineers around the world [w125] . [w120] . [w125] . [w125]
 [2br] This is the story of Adam, [w350] and how he got the best job ever. [w500]`
 
 let page1 =
 `Say hi to Adam! [w2250] [2br]
-Hah! I know what you’re thinking [w125] . [w125] . [w125] . [w300]
-[2br] “Adam?! Why, that’s my name, too! [w350] [br] This must be fate!" [w650]
+Hah! [w350] I know what you’re thinking: [w350]
+[2br] “Adam?! [w350] Why, that’s my name, too! [w350] [br] This must be fate!" [w650]
 [2br] Slow down, [w100] friendly reader! [w500]
 [2br] The story has barely begun, [w350] [br] and there’s much more to discover!`
 
 let page2 =
-`One boring spring day, [w100] ~
-Adam decided to take a gander at the available programming jobs on the web. ~
-Just then [w500] . [100] . [100] . [tA1]
-~
-[reactjs]
-~
-The most perfect job popped right up on the screen before his very eyes! [w220]
-~ “Wow!” he exclaimed. [w400]
-~ “There’s never been a position I’ve been more hopeful to have in my whole life!” he said to his ever-supportive cats.`
+`One boring spring day, [w100]
+Adam decided to take a gander at the available programming jobs on the web.
+[2br] Just then [w500] . [100] . [100] . [tA1]
+[ding]
+[2br] The most perfect job popped right up on the screen before his very eyes! [w220]
+[2br] “Wow!” he exclaimed. [w400]
+ “There’s never been a position I’ve been more hopeful to have in my whole life!” he said to his ever-supportive cats.`
 
 let page3 =
-`Behold! ~
-Adam is a front-end engineer who just so happens to specialize in [w80] . [w80] . [w80] . [w350]
-~ $ ~ [w500]
-“Crazy, that’s exactly what I’m looking for in a developer!” ~
-[w375] You’re undoubtedly thinking to yourself.
-~ BUT WAIT. [w250] ~ There’s more!`
+`BEHOLD!
+[2br] Adam is a front-end engineer who just so happens to specialize in [w80] . [w80] . [w80] . [w350]
+[reactjs]
+[2br] [w350] “Crazy, [w350] that’s exactly what I’m looking for in a developer!”
+[w350] You’re undoubtedly thinking to yourself.
+[2br] BUT WAIT. [w250] There’s more!`
 
 let page4 =
-`Adam also does back-end engineering, most often using node.js [w200]
-~ He feels at home with the ones and zeroes. [w150]
-~ Of course, [w80] the most important thing about Adam is that he loves to learn. [w200]
-~ Self-teaching is no foreign territory to this one!
-~ Constantly striving to expand his knowledge, Adam loves to take on side projects with coding friends on the weekends just to keep learning.
-~ [w200] “What a guy!” [w150] You’re saying it out loud, now.`
+`Adam *also* does back-end engineering, most often using node.js! [w350]
+He feels at home with the ones and zeroes. [w150]
+[2br] Of course, [w100] the most important thing about Adam is that he loves to learn. [w200]
+Self-teaching is no foreign territory to this one! [w100]
+[2br] Constantly striving to expand his knowledge, [w100] Adam loves to take on side projects with coding friends on the weekends just to keep learning.
+[2br] [w200] “What a guy!” [w350] You’re saying it out loud, [w50] now.`
 
 let page5 =
-`I, [w150] -AHEM- [w50] Adam,
-~ Isn’t exactly an artist [w80] . [w80] . [w80] .
-~ But, [w40] he does have ample experience in UX and UI design and development!
-~ Often working with only a small dev team or even solo,
-Adam frequently finds himself iterating designs alone or with a partner, and has learned a lot about it as a result!
-~ Adam always does his best to keep up with modern design principles, [w60] you know, [w60] to avoid looking like a time-traveling developer.`
+`I, [w350] -AHEM- [w150] Adam
+Isn’t exactly an artist [w80] . [w80] . [w80] .
+But, [w100] he does have ample experience in UX and UI design and development!
+[2br] Often working with only a small dev team or even solo,[w100]
+Adam frequently finds himself iterating designs alone or with a partner,[w100] and has learned a lot about it as a result!
+[2br] He always does his best to keep up with modern design principles, [w60] you know, [w60] to avoid looking like a time-traveling developer.`
 
 let page6 =
-`The most recent team Adam has had the pleasure of developing with has workers based around the globe!
-~ [w80] Naturally, [w80] he’s no stranger to a remote-working environment,
-and, perhaps the most exciting bit, [w100] Adam is located in
-~ < [w250]
-~ > [w250]
-~ = [w250]
-~ you mean [w15] . [w15] . [w15] . [w30]
-“I’d never have to be lonely again?!?” [w200]
-~ Yes, [w20] exactly!!`
+`The most recent team Adam has had the pleasure of developing with has workers based around the globe,
+[w100] so he’s no stranger to a remote-working environment. [w350]
+[2br] And,[w100] perhaps the most exciting bit, [w100] Adam is located in. [w20] . [w20] . [w20]
+[2br] EASTERN. [w450] STANDARD. [w450] TIME!!! [w450]
+[2br] . [w15] . [w15] . [w15] you mean [w15] . [w15] . [w15] . [w100]
+I'D NEVER BE LONELY AGAIN?!?” [w200]
+[2br] Yes, [w20] exactly!!`
 
 let page7 = `page7`
 
