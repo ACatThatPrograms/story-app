ff886fcc2b54916574464bdabfde4e209049ef93
acatthatprograms <acatthatprograms@gmail.com>
2020-05-27 18:20:32 -0400

    Narrative

--- src/util/narrative.js
+++ src/util/narrative.js
@@ -1,5 +1,5 @@
 let page0 =
-`Hello there! [2br] [wi] [w350] Have a seat, I've got a story for you! [w350] [wi]
+`Hello there! [2br] [w350] Have a seat, I've got a story for you! [w350] [wi]
 [2br] Now, [w350] here’s a tale worthy enough for the bedtimes of all young software engineers around the world [w125] . [w120] . [w125] . [w125]
 [2br] This is the story of Adam, [w350] and how he got the best job ever. [w500]`
 
@@ -7,46 +7,46 @@ let page1 =
 `Say hi to Adam! [2br]
 [w750] [f1] [w250] [f2] [w250] [f3] [w250]
 Hah! [w350] I know what you’re thinking: [w350]
-[2br] “Adam?! [w350] Why, that’s my name, too! [w350] [br] This must be fate!" [w650]
+[2br] “Adam?! [w350] Why, that’s my name, too! [w350] This must be fate!" [w650]
 [2br] Slow down, [w100] friendly reader! [w500]
 [2br] The story has barely begun, [w350] [br] and there’s much more to discover!`
 
 let page2 =
 `One boring spring day, [w100]
 Adam decided to take a gander at the available programming jobs on the web.
-[2br] Just then [w500] . [100] . [100] . [f1]
+[2br] Just then [w500] . [100] . [100] .
 [br] [ding] [br]
-[2br] The most perfect job popped right up on the screen before his very eyes! [w220]
+[2br] [f1] The most perfect job popped right up on the screen before his very eyes! [w220]
 [2br] “Wow!” he exclaimed. [w400]
  “There’s never been a position I’ve been more hopeful to have in my whole life!” he said to his ever-supportive cats.`
 
 let page3 =
-`BEHOLD!
+`BEHOLD! [w350]
 [2br] Adam is a front-end engineer who just so happens to specialize in [w80] . [w80] . [w80] . [w350]
 [2br] [reactjs]
-[2br] [w350] “Crazy, [w350] that’s exactly what I’m looking for in a developer!” [br]
+[2br] [w350] “Crazy, [w350] that’s exactly what I’m looking for in a developer!” [br] [w350]
 [w350] You’re undoubtedly thinking to yourself.
 [2br] BUT WAIT. [w250] There’s more!`
 
 let page4 =
-`Adam *also* does back-end engineering, most often using node.js! [w350] [br]
-He feels at home with the ones and zeroes. [w150]
-[2br] Of course, [w100] the most important thing about Adam is that he loves to learn. [w200] [br]
+`Adam *also* does back-end engineering, most often using node.js! [w350]
+He feels at home with the ones and zeroes. [w350]
+[2br] Of course, [w100] the most important thing about Adam is that he loves to learn. [w200]
 Self-teaching is no foreign territory to this one! [w100]
 [2br] Constantly striving to expand his knowledge, [w100] Adam loves to take on side projects with coding friends on the weekends just to keep learning.
 [2br] [w200] “What a guy!” [w350] You’re saying it out loud, [w50] now.`
 
 let page5 =
-`I, [w350] -AHEM- [w150] Adam [br]
-Isn’t exactly an artist [w80] . [w80] . [w80] . [br]
+`I, [w350] -AHEM- [w150] Adam
+isn’t exactly an artist [w80] . [w80] . [w80] . [br]
 But, [w100] he does have ample experience in UX and UI design and development!
 [2br] Often working with only a small dev team or even solo,[w100]
 Adam frequently finds himself iterating designs alone or with a partner,[w100] and has learned a lot about it as a result!
 [2br] He always does his best to keep up with modern design principles, [w60] you know, [w60] to avoid looking like a time-traveling developer.`
 
 let page6 =
-`Adam has had the pleasure of developing with workers based around the globe. [br]
-[w100] Naturally, he’s no stranger to a remote-working environment. [w350]
+`Adam has had the pleasure of developing with workers based around the globe,
+[w100] so he’s no stranger to a remote-working environment. [w350]
 [2br] And,[w100] perhaps the most exciting bit, [w100] Adam is located in. [w20] . [w20] . [w20]
 [2br] EASTERN. [w450] STANDARD. [w450] TIME!!! [w450]
 [2br] ". [w15] . [w15] . [w15] you mean [w15] . [w15] . [w15] . [w100]
@@ -55,7 +55,7 @@ I'D NEVER BE LONELY AGAIN?!?” [w200]
 
 let page7 = `So,[w100] Adam scrambled to apply to this once-in-a-lifetime opportunity. [w350]
 
-[2br] After all,[w50] how often does a developer run into a job listing like this? [w350] [2br]
+[2br] After all,[w50] how often does a developer run into a job listing like this? [w350]
 Full remote,[w50] a team that seems fun and agreeable,[w50] good pay,[w50] and requirements that he ticks all the boxes for? [w200] Awesome! [w350]
 [2br] He worked tirelessly to create his masterpiece. [w300] Through storms. [w300] Through hail. [w300] There was a blizzard.
 [w300] Then a drought. [w300] Then a heatwave, [w50] and his office air conditioner was broken. [w350]
@@ -70,13 +70,13 @@ let page8 = `Adam sat at his desk and became. [w50] . [w50] . worried. [w350]
 [br] “Did another applicant do the same?” [w250]
 [br] “What if they never reply?” [w250]
 
-But then. [w50] . [w50] . [w350] [f1]
+But then. [w50] . [w50] . [w350]
 
-[2br] [ding]
+[2br] [ding] [f1]
 
-[2br] He got an email!
+[2br] He got an email! [wi]
 
-[br] He got the interview!
+[br] He got the interview! [wi]
 
 [br] He got the job! [f2]
 
@@ -87,11 +87,12 @@ But then. [w50] . [w50] . [w350] [f1]
 let page9 =
 `If you would like to make this story a reality [w100] . [w100 . [100] . [br]
 Go ahead and [email] [2br]
-And if you like, [w100] don't forget to check out my [website] and [resume] ! [2br]
+And, [w100] don't forget to check out my [website] and [resume] ! [2br]
 Features: [br]
 - This awesome homebrew text box [br]
 - Custom code triggered narrative language [br]
-- Simple animation triggers utilizing CSSTransitionGroups
+- Simple animation triggers utilizing CSSTransitionGroups [br]
+- Crunch build time of about two days
 [2br] Thanks for your time!
 [br] Have a great day! [stop]
 `
